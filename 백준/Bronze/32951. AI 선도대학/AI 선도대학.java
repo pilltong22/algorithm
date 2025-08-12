ea236d0bc116880d94fcfa434cad2382390355b6
pilltong22 <106594632+pilltong22@users.noreply.github.com>
2025-08-12 17:18:58 +0900

    [Bronze V] Title: AI 선도대학, Time: 172 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int N;
+        int answer;
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        answer = N-2024;
+        System.out.println(answer);
+    }
+}
