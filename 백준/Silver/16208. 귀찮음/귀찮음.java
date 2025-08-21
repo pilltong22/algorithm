f4dc2b2ca5129018f5e710db838c24bba36770fd
pilltong22 <106594632+pilltong22@users.noreply.github.com>
2025-08-21 18:32:58 +0900

    [Silver V] Title: 귀찮음, Time: 172 ms, Memory: 17624 KB, Score: 4 point -BaekjoonHub

---
+++ 백준/Silver/16208. 귀찮음/귀찮음.java
@@ -0,0 +1,30 @@
+
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        int[] arr =  new int[n];
+        int sum =0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            sum+=arr[i];
+        }
+
+        Arrays.sort(arr);
+
+        int answer = 0;
+
+        for(int i=0 ;i<n-1;i++){
+            sum = sum -arr[i];
+            answer += arr[i] * sum;
+        }
+
+        System.out.println(answer);
+    }
+}
